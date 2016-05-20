4d7b8078a36e3138c10705f0ba78795cc0418c28
Jakub Tomanik <j.tomanik@gmail.com>
2016-05-20 15:28:56 +0200

    Proposal how to define protocols.

---
+++ Sources/Models/ModelProtocols.swift
@@ -0,0 +1,22 @@
+//
+//  ModelProtocols.swift
+//  Slacket
+//
+//  Created by Jakub Tomanik on 20/05/16.
+//
+//
+
+import Foundation
+
+// https://getpocket.com/developer/docs/v3/add
+protocol PocketItemType {}
+
+// https://api.slack.com/slash-commands
+protocol SlackCommandType {}
+
+// https://api.slack.com/docs/formatting
+protocol SlackResponseType {}
+
+protocol SlacketUserType {}
+
+protocol SlacketCommandType {}
---
+++ Sources/Services/ServiceProtocols.swift
@@ -0,0 +1,17 @@
+//
+//  ServiceProtocols.swift
+//  Slacket
+//
+//  Created by Jakub Tomanik on 20/05/16.
+//
+//
+
+import Foundation
+
+protocol PocketService {}
+
+protocol SlackService {}
+
+protocol OAuthService {}
+
+protocol APIClient {}
