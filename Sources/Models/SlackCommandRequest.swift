98ade2fe833b33b276c03dea2989d84177118d65
bartlomiejn <kontakt@bartlomiej-nowak.pl>
2016-05-22 17:21:21 +0200

    Updated SlackCommandRequest with Command model

--- Sources/Models/SlackCommandRequest.swift
+++ Sources/Models/SlackCommandRequest.swift
@@ -1,8 +1,24 @@
 struct SlackCommandRequest {
     
-    let user_name: String
-    let user_id: String
     let token: String
+    let teamId: String
+    let teamDomain: String
+    let channelId: String
+    let channelName: String
+    let userId: String
+    let userName: String
     let command: Command
-    let response_url: String
+    let responseUrl: String
+    
+    init(token: String, teamId: String, teamDomain: String, channelId: String, channelName: String, userId: String, userName: String, command: String, text: String, responseUrl: String) {
+        self.token = token
+        self.teamId = teamId
+        self.teamDomain = teamDomain
+        self.channelId = channelId
+        self.channelName = channelName
+        self.userId = userId
+        self.userName = userName
+        self.command = Command(name: command, parametersString: text)
+        self.responseUrl = responseUrl
+    }
 }
