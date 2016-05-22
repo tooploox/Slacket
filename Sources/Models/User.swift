59552c8b50b1c04dfb7dd42be76830af714d7c2b
bartlomiejn <kontakt@bartlomiej-nowak.pl>
2016-05-22 17:38:25 +0200

    Added User, UserProfile

---
+++ Sources/Models/User.swift
@@ -0,0 +1,12 @@
+struct User {
+    
+    let id: String
+    let name: String
+    let color: String
+    let profile: UserProfile
+    let deleted: Bool
+    let isAdmin: Bool
+    let isOwner: Bool
+    let has2fa: Bool
+    let hasFiles: Bool
+}
---
+++ Sources/Models/UserProfile.swift
@@ -0,0 +1,14 @@
+struct UserProfile {
+    
+    let firstName: String
+    let lastName: String
+    let realName: String
+    let email: String
+    let skype: String
+    let phone: String
+    let image24Url: String
+    let image32Url: String
+    let image48Url: String
+    let image72Url: String
+    let image192Url: String
+}
