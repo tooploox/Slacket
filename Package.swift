1b78f50375784cc6c594992ce78b65eb07b78208
Jakub Tomanik <j.tomanik@gmail.com>
2016-06-16 12:49:28 +0200

    Version bump

--- Package.swift
+++ Package.swift
@@ -8,8 +8,8 @@ let package = Package(
                     dependencies: [])
     ],
     dependencies: [
-                      .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 0, minor: 11),
-                      .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 0, minor: 6),
+                      .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 0, minor: 17),
+                      .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 0, minor: 9),
                       .Package(url: "https://github.com/swift-api/simple-http-client-swift", majorVersion: 0, minor: 1)
     ],
     exclude: ["Makefile", "Kitura-Build"]
