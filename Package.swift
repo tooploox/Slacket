46efba01e8735213d5a936283007a08542136536
Jakub Tomanik <j.tomanik@gmail.com>
2016-06-20 16:18:55 +0200

    rolled back

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     ],
     dependencies: [
                       .Package(url: "https://github.com/qutheory/libc.git", majorVersion: 0, minor: 1),
-                      .Package(url: "https://github.com/swift-api/Kitura.git", majorVersion: 0, minor: 17),
+                      .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 0, minor: 17),
                       .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 0, minor: 9),
                       .Package(url: "https://github.com/swift-api/simple-http-client-swift.git", majorVersion: 0, minor: 2),
                       .Package(url: "https://github.com/jtomanik/Environment.git", majorVersion: 0, minor: 3)
