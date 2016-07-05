b9e8d98f3a8c85da6746e1e4381177e190858a3b
Jakub Tomanik <j.tomanik@gmail.com>
2016-07-05 17:02:51 +0200

    Revert "Feature/mustache templates"

--- Sources/Slacket/Modules/Module.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Module.swift
-//  Slacket
-//
-//  Created by Jakub Tomanik on 31/05/16.
-//
-//
-
-import Foundation
--- Tests/slacket-health-check.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-curl -i 'http://127.0.0.1:8090/health-check'
