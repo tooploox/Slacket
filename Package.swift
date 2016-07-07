ee76d2022deccc037ce870f4479ccba9aa913a84
Jakub Tomanik <j.tomanik@gmail.com>
2016-07-07 18:21:58 +0200

    Changed Redis driver to https://github.com/czechboy0/Redbird

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         .Package(url: "https://github.com/jtomanik/Environment.git", majorVersion: 0, minor: 3),
         .Package(url: "https://github.com/tooploox/Kitura-HealthCheck.git", majorVersion: 0, minor: 2),
         .Package(url: "https://github.com/Zewo/Mustache.git", majorVersion: 0, minor: 6),
-        .Package(url: "https://github.com/chiahuang/Kitura-redis", majorVersion: 0, minor: 18)
+        .Package(url: "https://github.com/czechboy0/Redbird.git", majorVersion: 0, minor: 7)
     ],
     exclude: ["Makefile", "Kitura-Build"]
 )
